9d974c91b591c5eaef9fb7987b3aaa4f6323225e
Vladyslav Arseniuk <vladyslav.arseniuk@macys.com>
2023-06-02 11:31:42 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Vladyslav Arseniuk on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
